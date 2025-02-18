1ba518f6c80db80f6db2c5365093bb0bbca60073
Felix Warren <felix.warren@gmail.com>
2025-02-18 21:56:34 +0100

    commit innan jag introducerar funktion

--- reactorcontrol.lua
+++ reactorcontrol.lua
@@ -12,6 +12,7 @@ local lastlevel = percentenergy
 while true do
     storedenergy = reactor.getEnergyStored()
     percentenergy = math.ceil(storedenergy / 100000)
+    newlevel = percentenergy
 
     print("Reactor has " .. storedenergy .. " RF")
     print("That is " .. percentenergy .. " % of total capacity")
@@ -22,27 +23,27 @@ while true do
     if (newlevel - lastlevel) < -5 then
         reactor.setAllControlRodLevels(0)
         print("Energy level dropping quick, control rods fully retracted")
-        sleep(10)
 
     elseif percentenergy <= 50 then
         reactor.setAllControlRodLevels(0)
         print("Energy level below 50%, control rods fully retracted")
-        sleep(2)
+
     elseif percentenergy > 50 and percentenergy <= 90 then
         print("Normal operation. Control rods at energy level")
         reactor.setAllControlRodLevels(percentenergy)
-        sleep(10)
+
     elseif percentenergy > 90 then
         reactor.setAllControlRodLevels(100)
         print("Energy level high. Control rods fully inserted")
-        sleep(20)
+        sleep(10)
+
     else
         print("No change in control rods")
-        sleep(5)
+
     end
 
+    sleep(10)
     lastlevel = newlevel
-    newlevel = percentenergy
 
     print()
 
@@ -52,4 +53,4 @@ end
 
 
 -- rod change behöver egentligen ngn mer långsiktig variabel att rätta sig efter, typ trend i tid snarare än iteration av if-loopen
-
+-- kanske medel av fem senaste ändringarna? kan man göra det i en array som lägger till en och sparkar ut en? Funktion för det? 
