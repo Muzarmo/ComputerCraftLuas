d01d8124df500e1d8a0fc8c66a40102964a60edd
Felix Warren <felix.warren@gmail.com>
2025-02-19 07:33:52 +0100

    ändrat till trendvalue med leveldiff

--- reactorcontrol.lua
+++ reactorcontrol.lua
@@ -2,9 +2,9 @@
 -- Base idea is to lower control rods to the current reactor energy buffer level as way of a simple control mechanism.
 -- Being expanded upon since then
 
-local function calcTrend(energylevels, newlevel)
+local function calcTrend(energylevels, leveldiff)
     table.remove(energylevels, 1)
-    table.insert(energylevels, newlevel)
+    table.insert(energylevels, leveldiff)
 
     local sum = 0
 
@@ -16,8 +16,7 @@ local function calcTrend(energylevels, newlevel)
 end
 
 
-local energylevels = {100, 100, 100, 100, 100}
-
+local energylevels = {0, 0, 0, 0, 0}
 local reactor = peripheral.wrap("back")
 local storedenergy = reactor.getEnergyStored()
 local percentenergy = math.ceil(storedenergy / 100000)
@@ -30,28 +29,32 @@ while true do
     percentenergy = math.ceil(storedenergy / 100000)
     newlevel = percentenergy
 
-    trendvalue = calcTrend(energylevels, newlevel)
+    leveldiff = newlevel - lastlevel
+    trendvalue = calcTrend(energylevels, leveldiff)
     print("Trendvalue is " .. trendvalue)
-
     print("Reactor has " .. storedenergy .. " RF")
     print("That is " .. percentenergy .. " % of total capacity")
     print("Newlevel is " .. newlevel)
     print("Lastlevel is " .. lastlevel)
-    print("Newlevel - lastlevel is " .. (newlevel - lastlevel))
+    print("Leveldiff " .. leveldiff)
 
-    if (newlevel - lastlevel) < -5 then
+    if leveldiff < -5 then
         reactor.setAllControlRodLevels(0)
-        print("Energy level dropping quick, control rods fully retracted")
+        print("Energy level dropping quick. Control rods fully retracted")
+
+    elseif trendvalue < -3 then
+        reactor.setAllControlRodLevels(0)
+        print("Energy is trending downwards. Control rods fully retracted")
 
     elseif percentenergy <= 50 then
         reactor.setAllControlRodLevels(0)
-        print("Energy level below 50%, control rods fully retracted")
+        print("Energy level below 50%. Control rods fully retracted")
 
-    elseif percentenergy > 50 and percentenergy <= 90 then
+    elseif percentenergy > 50 and percentenergy <= 95 then
         print("Normal operation. Control rods at energy level")
         reactor.setAllControlRodLevels(percentenergy)
 
-    elseif percentenergy > 90 then
+    elseif percentenergy > 95 then
         reactor.setAllControlRodLevels(100)
         print("Energy level high. Control rods fully inserted")
         sleep(10)
@@ -72,4 +75,5 @@ end
 -- rod change behöver egentligen ngn mer långsiktig variabel att rätta sig efter, typ trend i tid snarare än iteration av if-loopen
 -- blivit lite förvirrad vad det var jag ville med trendvalue egentligen. jag tror att jag tänkte mig att ha ett trendvalue som jämförde diffen på level-values
 -- - men det kanske är smartare att göra som chatgpt ville. eller på något annat sätt?
+-- Nej, jag gör som jag ville och ser hur det faller ut.
 
