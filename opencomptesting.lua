11e43d4dafe62ec0b14c6b32180fa2a480a7b5f0
Felix Warren <felix.warren@gmail.com>
2025-03-23 23:35:18 +0100

    gått över till OpenComputers
    Det var svårt med NBT-taggar med ComputerCraft, så det blir OC framöver.

---
+++ opencomptesting.lua
@@ -0,0 +1,36 @@
+
+
+cognitojar = component.proxy("f53d07dc-322f-4bc2-a7e8-e3195812ac74")
+
+victusjar = component.proxy("b48a7b5f-084e-446c-857c-b4cf9d66ec4b")
+
+ordojar = component.proxy("76b3de14-73f0-49cd-85e5-1ff4111f9c50")
+
+alienisjar = component.proxy("787a1c51-2249-4869-a6ce-60904ac7274c")
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- aspects3 = jar3.getAspects()
+--
+-- for aspect, amount in pairs(aspects3) do
+--     print("jar3")
+--     print(aspect .. ": " .. amount)
+-- end
+--
+-- aspects4 = jar4.getAspects()
+--
+-- for aspect, amount in pairs(aspects4) do
+--     print("jar4")
+--     print(aspect .. ": " .. amount)
+--     end
+
