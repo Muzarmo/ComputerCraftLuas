a24e119a4e9493130a0a0d84ee1fcd7b6545ff60
Felix Warren <felix.warren@gmail.com>
2025-04-02 23:58:06 +0200

    Många ändringar, men främst i add_aspect
    add_aspect är en bra bit mot att vara omskriven. Har även gjort
    ändringar i menyn, la till en debug-funktion. La också till ett
    hjälp-table för att add_aspect ska funka. Smeltery type tas också med i
    beräkningen, och man kan ändra smeltery type från menyn.

--- jar_refill.lua
+++ jar_refill.lua
@@ -1,6 +1,11 @@
 
 local component = require("component")
 local potential_aspects = {"Cognitio", "Alienis", "Ordo", "Victus"}
+local aspect_add_data = {
+	Cognitio = {"minecraft:paper", 2},
+	Alienis = {"minecraft:ender_pearl", 10},
+	Ordo = {"minecraft:stonebrick", 1}
+}
 
 local void_jars = {}
 local normal_jars = {}
@@ -11,7 +16,7 @@ local chestpickupside = 3
 local chestburnside = 2
 local size = transposer.getInventorySize(chestpickupside)
 local sourcechestinventory = {}
-local essentiasmelterytype = {"Thaumic", 1,11}
+local essentiasmelterytype = {"Thaumic", 1.11}
 
 
 -- flyttar saker från source chest till burn chest
@@ -24,23 +29,27 @@ local function stuff_mover(thing, amount)
 			transposer.transferItem(chestpickupside, chestburnside, amount, slot, slot)
 			break
 		elseif stack and stack["name"] == thing and stack["size"] < amount then
-			print("Hittade del av " .. thing .. ". Delflyttar och fortsätter leta!")
+			print("Hittade " .. stack["size"] .. thing .. ". Delflyttar och fortsätter leta!")
 			transposer.transferItem(chestpickupside, chestburnside, stack["size"], slot, slot)
 			amount = amount - stack["size"]
 		end
 	end
 end
 
+
+-- function för att nollställa aspect list
 local function reset_aspects()
 	aspectlist = {}
 	print("Aspect list reset")
 end
 
+
+-- function för att byta smelterytyp
 local function change_smeltery_type()
 	if essentiasmelterytype[1] == "Thaumic" then
-		essentiasmelterytype = {"Base", 1,25}
+		essentiasmelterytype = {"Base", 1.25}
 	elseif essentiasmelterytype[1] == "Base" then
-		essentiasmelterytype = {"Thaumic", 1,11}
+		essentiasmelterytype = {"Thaumic", 1.11}
 	end
 end
 
@@ -60,27 +69,58 @@ local function sum_aspects(jar)
 end
 
 
--- funktion för att lägga till aspekter
+-- nya funktionen för att lägga till aspekter
 local function add_aspect(aspect, add_amount)
 	print("Add-aspect function har blivit tillsagd att lägga till " .. add_amount .. " till " .. aspect)
-	if aspect == "Alienis" then
-		local nopearls = math.ceil(add_amount / 10)
-		stuff_mover("minecraft:ender_pearl", nopearls)
-		aspectlist["Alienis"] = aspectlist["Alienis"] + (nopearls * 10)
-		aspectlist["Ordo"] = aspectlist["Ordo"] + (nopearls * 7.5)
+	if aspect_add_data[aspect] then
+		local block_type, no_blocks = table.unpack(aspect_add_data[aspect])
+		no_blocks = no_blocks * essentiasmelterytype[2] * add_amount
+		print("Antal block att flytta med förbränningsgrad medräknad är " .. no_blocks .. " och avrundat blir det " .. math.floor(no_blocks))
+		print("Lägger även till " .. add_amount .. " till aspectlist för " .. aspect)
+		print("Före: " .. aspect .. ": " .. aspectlist[aspect])
+		aspectlist[aspect] = aspectlist[aspect] + add_amount
+		print("Efter: " .. aspect .. ": " .. aspectlist[aspect])
+
+		print(block_type .. ": " .. no_blocks)
 	end
-	if aspect == "Cognitio" then
-		local nopaper = math.ceil(add_amount / 2)
-		stuff_mover("minecraft:paper", nopaper)
-		aspectlist["Cognitio"] = aspectlist["Cognitio"] + (nopaper * 2)
-		aspectlist["Victus"] = aspectlist["Victus"] + (nopaper * 1.5)
-	end
-	if aspect == "Ordo" then
-		local nostonebrick = add_amount
-		stuff_mover("minecraft:stonebrick", nostonebrick)
-		aspectlist["Ordo"] = aspectlist["Ordo"] + nostonebrick
+	
+	-- det behövs också på något sätt läggas till bi-aspekterna
+	
+	for baseaspect, value in pairs(aspect_add_data) do
+		print(baseaspect)
+		local block, blockamount = table.unpack(value)
+		print(block .. ": " .. blockamount)
+
 	end
 end
+
+
+-- debugfunktionen
+local function debug()
+	add_aspect("Ordo", 12)
+end
+
+-- gamla funktionen för att lägga till aspekter
+-- local function add_aspect(aspect, add_amount)
+-- 	print("Add-aspect function har blivit tillsagd att lägga till " .. add_amount .. " till " .. aspect)
+-- 	if aspect == "Alienis" then
+-- 		local nopearls = math.ceil(add_amount / 10)
+-- 		stuff_mover("minecraft:ender_pearl", nopearls)
+-- 		aspectlist["Alienis"] = aspectlist["Alienis"] + (nopearls * 10)
+-- 		aspectlist["Ordo"] = aspectlist["Ordo"] + (nopearls * 7.5)
+-- 	end
+-- 	if aspect == "Cognitio" then
+-- 		local nopaper = math.ceil(add_amount / 2)
+-- 		stuff_mover("minecraft:paper", nopaper)
+-- 		aspectlist["Cognitio"] = aspectlist["Cognitio"] + (nopaper * 2)
+-- 		aspectlist["Victus"] = aspectlist["Victus"] + (nopaper * 1.5)
+-- 	end
+-- 	if aspect == "Ordo" then
+-- 		local nostonebrick = add_amount
+-- 		stuff_mover("minecraft:stonebrick", nostonebrick)
+-- 		aspectlist["Ordo"] = aspectlist["Ordo"] + nostonebrick
+-- 	end
+-- end
 
 
 -- funktion för att inventera jars (och transposer) och lagra deras adresser
@@ -95,8 +135,9 @@ local function jaradresses()
 			table.insert(normal_jars, jaradress)
 			local jar = component.proxy(jaradress)
 			sum_aspects(jar)
-		elseif jartype == "transposer" then
-			local transposer = component.proxy(jaradress)
+		-- elseif jartype == "transposer" then
+		-- 	local transposer = component.proxy(jaradress)
+		-- den här behövs nog inte alls, förrän jag har fler transposers
 		end
 	end
 	for _, aspect in ipairs(potential_aspects) do
@@ -126,30 +167,17 @@ end
 
 -- funktion för att fylla på jars utifrån behov
 local function refilljars()
-	if aspectlist["Alienis"] then
-		if aspectlist["Alienis"] < 200 then
-			print("Alienis är under 200!")
-			local add_amount = (250-aspectlist["Alienis"])
-			add_aspect("Alienis", add_amount)
-		end
-	end
-	if aspectlist["Cognitio"] then
-		if aspectlist["Cognitio"] < 200 then
-			print("Cognitio är under 200!")
-			local add_amount = (250-aspectlist["Cognitio"])
-			add_aspect("Cognitio", add_amount)
-		end
-	end
-	if aspectlist["Ordo"] then
-		if aspectlist["Ordo"] < 200 then
-			print("Ordo är under 200!")
-			local add_amount = (250-aspectlist["Ordo"])
-			add_aspect("Ordo", add_amount)
+	for aspect, amount in pairs(aspectlist) do
+		if amount < 200 then
+			print(aspect .. " är under 200!")
+			local add_amount = 250 - amount
+			add_aspect(aspect, add_amount)
 		end
 	end
 end
 
 
+-- funktion för att skriva vad den beräknade aspect list innehåller (dvs eg inte vad som finns i jars)
 local function printjarcontents()
 	for aspect, amount in pairs(aspectlist) do
 		print(aspect .. ": " .. amount)
@@ -160,12 +188,16 @@ end
 local function main()
 	jaradresses()
 	while true do
-		print([[Meny: 
+		print(string.format([[
+		
+Meny: 
 1. Samla adresser / indexera jars
 2. Visa innehåll i aspectlist
 3. Fyll på jars
 4. Indexera källkistan
-5. Byt Smelterytyp. Nuvarande: ]] .. essentiasmelterytype[1] .. "\n0. Avsluta")
+5. Byt Smelterytyp. Nuvarande: %s
+6. Debug 
+0. Avsluta]], essentiasmelterytype[1]))
 		io.write("Välj ett alternativ: ")
 		local choice = io.read()
 			if choice == "1" then
@@ -178,6 +210,8 @@ local function main()
 				sourcechest()
 			elseif choice == "5" then
 				change_smeltery_type()
+			elseif choice == "6" then
+				debug()
 			elseif choice == "0" then
 				break
 			else
@@ -188,6 +222,9 @@ end
 
 
 main()
+
+
+
 
 
 
@@ -207,11 +244,6 @@ main()
 
 
 -- Ideer
--- Variabel som man kan ändra från menyn med efficiency
---  - 0.8 eller 0.9 beroende på vilken sorts Essentia Smeltery man har
-
--- Multiplikator för 0.9: 1.11
--- Multiplikator för 0.8: 1.25
 --
 -- Behövs säkert städas i de floats som uppstår efteråt
 --
@@ -228,3 +260,6 @@ main()
 
 -- Göra en lista med vilken sak som ska adderas för att lägga till essentia för varje aspect, och göra en for loop av det? 
 -- - problem om man vill lägga till två alternativ? Går att få till? 
+
+-- Göra en key-value med display-motsvarigheter till minecraft-variablerna för block. Så att den skriver "Stone" istället för 
+-- minecraft:stone. 
